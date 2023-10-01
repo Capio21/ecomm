550abd3c8dea24e536c2e91dfbdc2818a3687248
Capio21 <130652957+Capio21@users.noreply.github.com>
2023-10-01 14:51:59 +0800

    ghj

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,5 +13,5 @@ use CodeIgniter\Router\RouteCollection;
      $routes->post('/create', 'AdminController::create');
      $routes->post('admins/edit/(:num)', 'AdminController::edit/$1');
      $routes->get('admins/delete/(:num)', 'AdminController::delete/$1');
-     $routes->get('Login/Register', 'UserController::register');
+     $routes->get('Login/Register', 'UserController::register' ,['filter' => 'authGuard']);
      $routes->get('Login/Signin', 'UserController::signin' ,['filter' => 'authGuard']); // You need to create this method in your controller
